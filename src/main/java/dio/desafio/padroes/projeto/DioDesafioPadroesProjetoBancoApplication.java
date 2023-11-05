c9cc333e3ac629dc3343a5dcc99cfbf77e1e54cb
Rlam-git <raul_k-22@hotmail.com>
2023-11-05 15:13:12 -0300

    Adicionado arquivos iniciais do projeto.

---
+++ src/main/java/dio/desafio/padroes/projeto/DioDesafioPadroesProjetoBancoApplication.java
@@ -0,0 +1,13 @@
+package dio.desafio.padroes.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioDesafioPadroesProjetoBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioDesafioPadroesProjetoBancoApplication.class, args);
+	}
+
+}
